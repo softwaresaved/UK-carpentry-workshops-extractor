cb72bcd89a011875201009d6f707826979e9c456
Aleksandra Nenadic <a.nenadic@manchester.ac.uk>
2017-02-14 09:49:35 +0000

    Added extracting longitude, latitude and number of attendees for workshops.

--- extract-UK-workshops.rb
+++ extract-UK-workshops.rb
@@ -7,8 +7,10 @@ require 'open-uri'
 require 'json'
 require 'csv'
 require 'fileutils'
+require 'nokogiri'
 
 amy_api_published_events_url = "https://amy.software-carpentry.org/api/v1/events/published/"
+amy_workshop_events_url = "https://amy.software-carpentry.org/workshops/event"
 
 search_results = []
 uk_workshops = []
@@ -24,8 +26,29 @@ else
   puts "Result stats: number of UK workshops = #{uk_workshops.length.to_s}; total number of all workshops = #{search_results.length.to_s}."
 end
 
+# Figure out the number of students from AMY records (from table listing people and roles, where role == 'learner') for each of the workshop.
+# Accessing pages requires user to be authenticated, hence we use the 'Cookie' header with session info which expires after a while.
+# Cookie header looks something like this: "Cookie" => "__utma=2571...; __utmc=257105289; sessionid=cyu3...; _ga=GA1.2...; csrftoken=MrTv..."
+# and you can figure one out if you look at what headers your browser sends after you are authenticated with AMY. This is a hackish way of
+# doing things but there is no other way to authenticate programatically at the moment. Nor is this info avaiable via the public API.
+uk_workshops.each do |workshop|
+  begin
+    # Replace the Cookie header info with the correct one, if you have access to AMY
+    workshop_html_page = Nokogiri::HTML(open(amy_workshop_events_url + "/" + workshop["slug"], "Cookie" => "__utma=2571...; __utmc=257105289; sessionid=cyu3...; _ga=GA1.2...; csrftoken=MrTv..."))
+    puts "Calculating the number of attendees for workshop #{workshop["slug"]}."
+    workshop['number_of_attendees'] = workshop_html_page.xpath('//table/tr/td[contains(text(), "learner")]').length
+    puts "Found #{workshop["number_of_attendees"]} attendees for #{workshop["slug"]}."
+  rescue Exception => ex
+    # Skip to the next workshop
+    puts "Failed to get number of attendees for workshop #{workshop["slug"]} from #{workshop["url"]}. An error of type #{ex.class} occurred, the reason being: #{ex.message}."
+    exit
+  end
+end
+
 # CSV headers
-csv_headers = ["slug", "humandate", "start", "end", "tags", "venue", "address", "url"]
+csv_headers = ["slug", "humandate", "start", "end", "tags", "venue", "address", "latitude", "longitude",
+               "number_of_attendees",
+               "eventbrite_id", "contact", "url"]
 
 date = Time.now.strftime("%Y-%m-%d")
 csv_file = "SWC-DC-TTT-workshops-UK_#{date}.csv"
@@ -44,6 +67,11 @@ begin
               workshop["tags"].map{|x| x["name"]}.join(", "),
               workshop["venue"],
               workshop["address"],
+              workshop["latitude"],
+              workshop["longitude"],
+              workshop["number_of_attendees"],
+              workshop["eventbrite_id"],
+              workshop["contact"],
               workshop["url"]]
     end
   end
