8d358b30dda2aa1f961b3d56c46a6e5a35a5b501
Aleksandra Nenadic <a.nenadic@manchester.ac.uk>
2017-11-14 10:14:58 +0000

    Refactored check missing coords script.

--- check_missing_coords.py
+++ check_missing_coords.py
@@ -1,22 +1,20 @@
 import os
 import pandas as pd
 import numpy as np
+import traceback
 
-DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
 
-FILE_NAME = DIR_PATH + '/lib/UK-academic-institutions-geodata.xlsx'
+UK_INSTITUTIONS_GEOCODES_FILE = CURRENT_DIR + '/lib/UK-academic-institutions-geodata.xlsx'
 
-def create_excel_file_dataframe(filename):
+def load_geocodes(filename):
     """
     Creates a dataframe for the corresponding excel file.
     """
-    try:
-        excel_file = pd.ExcelFile(filename)
-        return excel_file.parse('UK-academic-institutions')
-    except FileNotFoundError:
-        print('The file you were looking for is not found.')
-        
-def transform_missing_values(df):
+    excel_file = pd.ExcelFile(filename)
+    return excel_file.parse('UK-academic-institutions')
+
+def fix_missing_values(df):
     """
     Replaces missing nan values for empty strings.
     """
@@ -24,38 +22,39 @@ def transform_missing_values(df):
 
 def add_missing_coordinates(df):
     """
-    Adds missing know coordinates and makes a list of the institutions
-    that have missing coordinates.
+    Adds missing known coordinates (geocodes - latitude and longitude pairs) and make a list of the institutions that have missing coordinates.
     """
-    ## Missing known coordinates
-    miss_coords = [["The Queen's University of Belfast", -5.9348, 54.5839],
-                   ["St Mary's University College", -5.9613, 54.592],
-                   ['University of Ulster', -6.6725, 55.1468],
-                   ['Stranmillis University College', -5.9352, 54.5733]]
+    ## Known missing coordinates
+    known_missing_coords = {
+        "The Queen's University of Belfast" : [-5.9348, 54.5839],
+        "St Mary's University College" : [-5.9613, 54.592],
+        "University of Ulster" : [-6.6725, 55.1468],
+        "Stranmillis University College" : [-5.9352, 54.5733]
+    }
 
-    ## Missing institutions names
-    list_institutions = []
-    list_institutions_included = []
+    ## Institutions with missing coordinates after fixing
+    institutions_with_missing_coords = []
 
     ## Check for missing data in coordinates and add missing coords
     for index, row in df.iterrows():
         if row['LONGITUDE']=='':
-            list_institutions.append(row['VIEW_NAME'])
-            for each in miss_coords:
-                if each[0] not in list_institutions_included:
-                    list_institutions_included.append(each[0])
-                if each[0] == row['VIEW_NAME']:
-                    df.set_value(index,'LONGITUDE',each[1])
-                    df.set_value(index,'LATITUDE',each[2])
-                    
-    return df, list_institutions, list_institutions_included
+            if row['VIEW_NAME'] in known_missing_coords.keys() :
+                df.set_value(index,'LONGITUDE',  known_missing_coords[row['VIEW_NAME']][0])
+                df.set_value(index,'LATITUDE', known_missing_coords[row['VIEW_NAME']][1])
+                print("Fixing missing coordinates for " + row['VIEW_NAME'])
+            else:
+                institutions_with_missing_coords.append(row['VIEW_NAME'])
 
-def create_spreadsheet(df):
+    if len(institutions_with_missing_coords) > 0:
+        print('The institutions that are still missing the coordinates are: ' + str(institutions_with_missing_coords))
+
+    return df
+
+def save_geocodes(df, file):
     """
-    Create a new excel file with the missing known coordinates added.
+    Save dataframe to an Excel spreadsheet file.
     """
-    new_excel_file = DIR_PATH + '/lib/UK-academic-institutions-geodata_missing.xlsx'
-    writer = pd.ExcelWriter(new_excel_file , engine='xlsxwriter')
+    writer = pd.ExcelWriter(file , engine='xlsxwriter')
 
     df.to_excel(writer, sheet_name='UK-academic-institutions')
     writer.save()
@@ -64,15 +63,19 @@ def main():
     """
     Main function
     """
-    df = create_excel_file_dataframe(FILE_NAME)
-    df = transform_missing_values(df)
-    df = add_missing_coordinates(df)[0]
+    print('Checking institutions for missing coordinates (geocodes) ...')
+    try:
+        df = load_geocodes(UK_INSTITUTIONS_GEOCODES_FILE)
+        df = fix_missing_values(df)
+        df = add_missing_coordinates(df)
+        save_geocodes(df, UK_INSTITUTIONS_GEOCODES_FILE)
+    except FileNotFoundError:
+        print('The Excel file with geocodes for the UK institutions ' + UK_INSTITUTIONS_GEOCODES_FILE + 'was not found.')
+        print(traceback.format_exc())
+    except Exception:
+        print('Failed to write to the Excel file with geocodes for the UK institutions ' + UK_INSTITUTIONS_GEOCODES_FILE)
+        print(traceback.format_exc())
 
-    create_spreadsheet(df)
-    
-    print('The institutions missing are: ' + str(add_missing_coordinates(df)[1]))
-    print('The coordinates for the following institutions were added to the spreadsheet: '
-          + str(add_missing_coordinates(df)[2]))
 
 if __name__ == '__main__':
     main()
