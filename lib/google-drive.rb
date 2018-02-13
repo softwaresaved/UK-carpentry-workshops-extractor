e8eaee622f480c5098186046463d350e2a48e313
Aleksandra Nenadic <a.nenadic@manchester.ac.uk>
2018-02-13 10:12:14 +0000

    Added Google drive upload of extracted source data.

--- lib/google-drive.rb
+++ lib/google-drive.rb
@@ -1,13 +1,15 @@
 require 'google_drive'
+require "fileutils"
 
 def google_drive_upload(file_path, google_folder_id)
   begin
-    session = GoogleDrive::Session.from_config("client_secrets.json")
+    secrets_file = File.join(File.dirname(__FILE__), '/../client_secrets.json')
+    session = GoogleDrive::Session.from_config(secrets_file)
     file_name = File.basename(file_path)
     parent_folder = session.file_by_id(google_folder_id)
     parent_folder.upload_from_file(file_path,
                                    file_name,
-                                   {convert: false, :content_type => "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"})
+                                   {convert: false, :content_type => "text/csv"})
     puts "File #{file_path} uploaded to Google Drive."
   rescue Exception => ex
     puts "\n" + "#" * 80 +"\n\n"
