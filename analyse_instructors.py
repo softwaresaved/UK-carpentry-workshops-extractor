d7ec83a230c22eef22f1b3a00e41b678b808377c
Aleksandra Nenadic <a.nenadic@manchester.ac.uk>
2020-12-02 11:30:27 +0000

    Fix for analysing instuctors to match latest column names

--- analyse_instructors.py
+++ analyse_instructors.py
@@ -23,36 +23,24 @@ def main():
     """
     args = helper.parse_command_line_parameters_analyses()
 
-    if args.input_file:
-        instructors_file = args.input_file
-    else:
-        print("Trying to locate the latest CSV spreadsheet with Carpentry instructors to analyse in " + RAW_DATA_DIR)
-        instructors_files = glob.glob(RAW_DATA_DIR + "/carpentry-instructors_*.csv")
-        instructors_files.sort(key=os.path.getctime)  # order files by creation date
-
-        if not instructors_files:
-            print('No CSV file with Carpentry instructors found in ' + RAW_DATA_DIR + ". Exiting ...")
-            sys.exit(1)
-        else:
-            instructors_file = instructors_files[-1]  # get the last file
-
+    instructors_file = args.input_file
     instructors_file_name = os.path.basename(instructors_file)
     instructors_file_name_without_extension = re.sub('\.csv$', '', instructors_file_name.strip())
 
-    print('CSV file with Carpentry instructors to analyse ' + instructors_file)
+    print('CSV file with Carpentry instructors to be analysed ' + instructors_file)
 
     try:
         instructors_df = pd.read_csv(instructors_file, encoding="utf-8")
-        # instructors_df.drop(labels="email", axis=1, inplace=True) // We do not download personal data from AMY any more so nothing to drop here
+        # instructors_df.drop(labels="email", axis=1, inplace=True) // We do not download personal data any more so nothing to drop here
 
-        # Insert normalised/official names for institutions (for UK academic institutions)
-        instructors_df = helper.insert_normalised_institution(instructors_df, "affiliation")
-
-        # Insert latitude, longitude pairs for instructors' institutions
-        instructors_df = helper.insert_institutional_geocoordinates(instructors_df, "normalised_institution", "latitude", "longitude")
-
-        # Insert UK regional info based on instructors's affiliations or nearest airport
-        instructors_df = helper.insert_uk_region(instructors_df)
+        # # Insert normalised/official names for institutions (for UK academic institutions)
+        # instructors_df = helper.insert_normalised_institution(instructors_df, "institution")
+        #
+        # # Insert latitude, longitude pairs for instructors' institutions
+        # instructors_df = helper.insert_institutional_geocoordinates(instructors_df, "normalised_institution", "latitude", "longitude")
+        #
+        # # Insert UK regional info based on instructors's affiliations or nearest airport
+        # instructors_df = helper.insert_uk_region(instructors_df)
 
         if not os.path.exists(ANALYSES_DIR):
             os.makedirs(ANALYSES_DIR)
@@ -88,7 +76,7 @@ def instructors_per_year_analysis(df, writer):
     Number of instructors per year.
     """
     instructors_per_year = pd.core.frame.DataFrame(
-        {'number_of_instructors': df.groupby(['year-earliest-instructor-badge-awarded']).size()}).reset_index()
+        {'number_of_instructors': df.groupby(['year_earliest_badge_awarded']).size()}).reset_index()
     instructors_per_year.to_excel(writer,
                                   sheet_name='instructors_per_year',
                                   index=False)
@@ -181,7 +169,7 @@ def instructors_per_UK_region_analysis(df, writer):
     Number of instructors per UK region.
     """
     instructors_per_UK_region = pd.core.frame.DataFrame(
-        {'number_of_instructors': df.groupby(['region']).size().sort_values()}).reset_index()
+        {'number_of_instructors': df.groupby(['institutional_region']).size().sort_values()}).reset_index()
     instructors_per_UK_region.to_excel(writer,
                           sheet_name='instructors_per_region',
                           index=False)
