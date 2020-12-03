9dd9e0291459467cfebfdf6ce223c105d6c86412
Mario Antonioletti <mario@epcc.ed.ac.uk>
2020-12-03 10:35:35 +0000

    Modifications to run on a new VM.

--- cron/RunAnalysis.sh
+++ cron/RunAnalysis.sh
@@ -2,9 +2,12 @@
 #
 # Script to run Alek's analysis pipeline.
 
+# Add python to the path
+export PATH=$PATH:/home/mario/anaconda3/bin
+
 # Change to the correct directory - change for your system.
 echo Changing to the analysis directory
-cd /home/mario/carpentry-workshops-instructors-extractor
+cd /home/mario/carpentry-data-analysis
 echo
 
 # Make sure that we have the latest version of the code
@@ -12,32 +15,32 @@ echo Pulling from GitHub
 git pull
 echo
 
-# Run the data extraction script.
-echo Getting the data from Amy
-python3 amy_data_extract.py -c GB 
+# Define the data files
+WORKSHOPS_DATAFILE="data/processed/processed_carpentry_workshops_UK_$(date +'%Y-%m-%d')_redash.csv"
+INSTRUCTORS_DATAFILE="data/processed/processed_carpentry_instructors_UK_$(date +'%Y-%m-%d')_redash.csv"
+
+# Get the data from redash
+echo Getting the data
+python extract_and_process_redash.py -pw $WORKSHOPS_DATAFILE -pi $INSTRUCTORS_DATAFILE
 echo
 
-# Generate the input/output filenames for workshops.
-infile=carpentry-workshops_GB_$(date +"%Y-%m-%d").csv
-outfile1=analysed_carpentry-workshops_GB_$(date +"%Y-%m-%d").xlsx
+# Define the analysis files
+ANALYSED_WORKSHOPS="data/analyses/analysed_carpentry_workshops_UK_$(date +'%Y-%m-%d').csv"
+ANALYSED_INSTRUCTORS="data/analyses/analysed_carpentry_instructors_UK_$(date +'%Y-%m-%d').csv"
 
 # Produce workshop output data.
 echo Analysing the workshop data
-python3 analyse_workshops.py -in data/raw/${infile}
+python analyse_workshops.py -in $WORKSHOPS_DATAFILE -out $ANALYSED_WORKSHOPS
 echo
-
-# Generate the input/output filenames for instructors.
-infile=carpentry-instructors_GB_$(date +"%Y-%m-%d").csv
-outfile2=analysed_carpentry-instructors_GB_$(date +"%Y-%m-%d").xlsx
 
 # Produce instructor output data.
 echo Analysing the instructor data
-python3 analyse_instructors.py -in data/raw/${infile}
+python analyse_instructors.py -in $INSTRUCTORS_DATAFILE -out $ANALYSED_INSTRUCTORS
 echo
 
-# Push the processed data back to GitHub
+# Push the processed and analysed data back to GitHub
 date=$(date +"%Y-%m-%d")
-git add data/analyses/${outfile1} data/analyses/${outfile2}
+git add data/analyses/ data/processed data/raw
 git commit -m "Adding carpentry and workshop data for ${date}."
 git push
 
@@ -45,15 +48,15 @@ git push
 # hanging off the home directory. We will copy the output files to that
 # repository. Add them, commit them, do a git pull of the repo contents
 # and then push them to the metrics repository.
-cp ./data/analyses/${outfile1} ~/metrics/training/workshops/analyses
-cp ./data/analyses/${outfile2} ~/metrics/training/workshops/analyses
+#cp ./data/analyses/${outfile1} ~/metrics/training/workshops/analyses
+#cp ./data/analyses/${outfile2} ~/metrics/training/workshops/analyses
 
 # Go to the repository, add and commit the files.
-cd ~/metrics/training/workshops/analyses
-git add ${outfile1}
-git add ${outfile2}
-git commit -m "Adding carpentry and workshop data for ${date}."
+#cd ~/metrics/training/workshops/analyses
+#git add ${outfile1}
+#git add ${outfile2}
+#git commit -m "Adding carpentry and workshop data for ${date}."
 # Make sure the repo is up to date.
-git pull  
+#git pull  
 # Now push the results.
-git push 
+#git push 
